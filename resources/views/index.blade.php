c93db59e5da1f3e2ad22ad65a92f7a015686742b
EJGoose <solangoose@duck.com>
2025-07-31 11:45:29 +0100

    Update readme

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -41,7 +41,7 @@
                             <input name='q' placeholder="Search" value type='text' id='q' @input.debounce.500ms="submit()" :placeholder='loadingContent ? "Searching..." : "Search"' aria-label="Search" x-model="searchQuery" class="border-gray-300 placeholder-gray-400 text-gray-800 text-base block w-full rounded-1-md pl-14 focus:outline-none focus:ring-0">
                             <div class="bg-gray-100 border border-gray-300 border-1-0 flex items-center justify-center px-3 rounded-r-md text-gray-700 select-none">
                                 <span class="whitespace-nowrap space-x-1 flex items-center">
-                                    <button @click.prevent ="currentPage--; $nextTick(() => submit())" :class="{'text-gray-200' : currentPage < 2}" class="h-6 rounded-md text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-800" :disabled='currentPage < 2'>
+                                    <button @click.prevent ="currentPage--; $nextTick(() => submit())" :class="{'text-gray-200' : currentPage < 2}" class="h-6 rounded-md text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-800 hover:scale-110" :disabled='currentPage < 2'>
                                         {{-- left arrow icon --}}
                                         <span class="w-6 h-6 inline-block">
                                             <svg class="w-full" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
@@ -52,7 +52,7 @@
                                             of 
                                         <span class="text-gray-600" x-text='activeData.pageEnd'></span>
                                     </span>
-                                    <button @click.prevent="currentPage++; $nextTick(() => submit())" :class="{'text-gray-200': !activeData.more}" class="h-6 text-blue-700 focus:ring-2 focus:ring-blue-800 rounded-md focus:outline-none" :disabled='!activeData.more'>
+                                    <button @click.prevent="currentPage++; $nextTick(() => submit())" :class="{'text-gray-200': !activeData.more}" class="h-6 text-blue-700 focus:ring-2 focus:ring-blue-800 rounded-md focus:outline-none hover:scale-110" :disabled='!activeData.more'>
                                         {{-- right arrow icon --}}
                                         <span class=" w-6 h-6 inline-block">
                                             <svg class="w-full" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
